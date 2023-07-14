3853696140ccdb8f9fc462fcc853560cf73ebc4d
odeyalo <theprogrammerinfuture@gmail.com>
2023-07-14 18:38:00 +0300

    Updating project [com.odeyalo.sonata:authentication:0.0.1-SNAPSHOT] with stubs

---
+++ META-INF/com.odeyalo.sonata/authentication/0.0.1-SNAPSHOT/contracts/password/shouldUpdatePasswordAndReturnOk.groovy
@@ -0,0 +1,31 @@
+package contracts.password
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method PUT()
+        urlPath("/auth/settings/password-change") {
+            queryParameters {
+                parameter "userId": value(1)
+            }
+        }
+        headers {
+            contentType "application/json"
+        }
+        body(
+                [
+                        "old_password": "old_password_123",
+                        "new_password": "neW_password123"
+                ]
+        )
+    }
+    response {
+        status OK()
+        body(
+                [
+                        "updated": true
+                ]
+        )
+    }
+}
